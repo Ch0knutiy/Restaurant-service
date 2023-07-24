5d3b52ac8b6639b10acb636d2b11a91aeca45558
George Ustyanchik <ghkro123@gmail.com>
2023-07-24 17:44:32 +0700

    add README.md

--- database.py
+++ database.py
@@ -3,7 +3,6 @@ from sqlalchemy import create_engine
 
 from models.models import Base
 
-
 engine = create_engine("postgresql+psycopg2://postgres:root@localhost/Y_lab")
 Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
