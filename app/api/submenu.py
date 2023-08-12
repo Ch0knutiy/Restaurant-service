5c164bf91914c462a84c2863b74ed0b2fbaa3179
George Ustyanchik <ghkro123@gmail.com>
2023-08-12 16:50:10 +0700

    rebuild to async

--- app/api/submenu.py
+++ app/api/submenu.py
@@ -1,7 +1,7 @@
 from uuid import UUID
 
 from database import get_db
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, BackgroundTasks, Depends, HTTPException, status
 from schemas import schemas
 from services import submenuService
 from sqlalchemy.orm import Session
@@ -11,12 +11,12 @@ router = APIRouter()
 
 @router.get('/{menu_id}/submenus')
 async def get_submenus(menu_id: UUID, db: Session = Depends(get_db)):
-    return submenuService.get_submenus(menu_id, db)
+    return await submenuService.get_submenus(menu_id, db)
 
 
 @router.get('/{menu_id}/submenus/{id}')
 async def get_submenu(id: UUID, db: Session = Depends(get_db)):
-    submenu = submenuService.get_submenu(id, db)
+    submenu = await submenuService.get_submenu(id, db)
     if not submenu:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail='submenu not found')
@@ -24,13 +24,15 @@ async def get_submenu(id: UUID, db: Session = Depends(get_db)):
 
 
 @router.post('/{menu_id}/submenus', status_code=201)
-async def create_submenu(payload: schemas.SubmenuSchema, menu_id: UUID, db: Session = Depends(get_db)):
-    return submenuService.create_submenu(payload, menu_id, db)
+async def create_submenu(payload: schemas.SubmenuSchema, menu_id: UUID, background_tasks: BackgroundTasks,
+                         db: Session = Depends(get_db)):
+    return await submenuService.create_submenu(payload, menu_id, background_tasks, db)
 
 
 @router.patch('/{menu_id}/submenus/{id}', status_code=200)
-async def update_submenu(id: UUID, menu_id: UUID, payload: schemas.SubmenuSchema, db: Session = Depends(get_db)):
-    submenu = submenuService.update_submenu(id, menu_id, payload, db)
+async def update_submenu(id: UUID, menu_id: UUID, payload: schemas.SubmenuSchema, background_tasks: BackgroundTasks,
+                         db: Session = Depends(get_db)):
+    submenu = await submenuService.update_submenu(id, menu_id, payload, background_tasks, db)
     if not submenu:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f'No submenu with this id: {id} found')
@@ -38,5 +40,5 @@ async def update_submenu(id: UUID, menu_id: UUID, payload: schemas.SubmenuSchema
 
 
 @router.delete('/{menu_id}/submenus/{id}', status_code=200)
-async def delete_submenu(id: UUID, menu_id: UUID, db: Session = Depends(get_db)):
-    return submenuService.delete_submenu(id, menu_id, db)
+async def delete_submenu(id: UUID, menu_id: UUID, background_tasks: BackgroundTasks, db: Session = Depends(get_db)):
+    return await submenuService.delete_submenu(id, menu_id, background_tasks, db)
--- app/services/submenuService.py
+++ app/services/submenuService.py
@@ -19,19 +19,19 @@ def clear_menu_cache(menu_id: UUID) -> None:
     repositoryCache.del_cache('menus' + str(menu_id))
 
 
-def enrich_submenu(submenu: models.Submenu, db: Session) -> schemas.EnrichedSubmenuSchema | None:
+async def enrich_submenu(submenu: models.Submenu, db: Session) -> schemas.EnrichedSubmenuSchema | None:
     if not submenu:
         return None
     external_data = {
         'id': submenu.id,
         'title': submenu.title,
         'description': submenu.description,
-        'dishes_count': submenuRepository.dishes_count(submenu.id, db)
+        'dishes_count': await submenuRepository.dishes_count(submenu.id, db)
     }
     return schemas.EnrichedSubmenuSchema(**external_data)
 
 
-def get_submenus(menu_id: UUID, db: Session) -> list[schemas.EnrichedSubmenuSchema]:
+async def get_submenus(menu_id: UUID, db: Session) -> list[schemas.EnrichedSubmenuSchema]:
     result = repositoryCache.get_cache('submenus' + str(menu_id))
     if not result:
         result = []
@@ -43,7 +43,7 @@ def get_submenus(menu_id: UUID, db: Session) -> list[schemas.EnrichedSubmenuSche
     return result
 
 
-def get_submenu(id: UUID, db: Session) -> schemas.EnrichedSubmenuSchema | None:
+async def get_submenu(id: UUID, db: Session) -> schemas.EnrichedSubmenuSchema | None:
     result = repositoryCache.get_cache('submenu' + str(id))
     if not result:
         result = enrich_submenu(submenuRepository.get_submenu(id, db), db)
@@ -53,18 +53,18 @@ def get_submenu(id: UUID, db: Session) -> schemas.EnrichedSubmenuSchema | None:
     return result
 
 
-def create_submenu(payload: schemas.SubmenuSchema, menu_id: UUID, db: Session) -> models.Submenu:
+async def create_submenu(payload: schemas.SubmenuSchema, menu_id: UUID, db: Session) -> models.Submenu:
     clear_menu_cache(menu_id)
     clear_submenu_cache(menu_id)
     return submenuRepository.create_submenu(payload, menu_id, db)
 
 
-def update_submenu(id: UUID, menu_id: UUID, payload: schemas.SubmenuSchema, db: Session) -> models.Submenu | None:
+async def update_submenu(id: UUID, menu_id: UUID, payload: schemas.SubmenuSchema, db: Session) -> models.Submenu | None:
     clear_submenu_cache(menu_id, id)
     return submenuRepository.update_submenu(id, payload, db)
 
 
-def delete_submenu(id: UUID, menu_id: UUID, db: Session) -> dict[str, bool]:
+async def delete_submenu(id: UUID, menu_id: UUID, db: Session) -> dict[str, bool]:
     clear_submenu_cache(menu_id, id)
     clear_menu_cache(menu_id)
     return submenuRepository.delete_submenu(id, db)
