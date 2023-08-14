45ea4740c6dc1f731a9d600c8e92da0f3121993a
George Ustyanchik <ghkro123@gmail.com>
2023-08-14 15:52:44 +0700

    async tests

--- app/redis_connection.py
+++ app/redis_connection.py
@@ -2,5 +2,5 @@ import redis.asyncio as redis
 
 
 async def get_redis():
-    return await redis.Redis(host='localhost', port=6379, decode_responses=True, )
-    # return await redis.Redis(host='y_lab_redis', port=6379, decode_responses=True, )
+    # return await redis.Redis(host='localhost', port=6379, decode_responses=True, )
+    return await redis.Redis(host='y_lab_redis', port=6379, decode_responses=True, )
