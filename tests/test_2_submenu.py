ac5a72f9a1691b44183af29fab0c64e4292d2c56
George Ustyanchik <ghkro123@gmail.com>
2023-08-14 15:47:47 +0700

    async tests

--- tests/test_2_submenu.py
+++ tests/test_2_submenu.py
@@ -1,6 +1,5 @@
-import httpx
+import pytest
 
-client = httpx.Client()
 uuidMenu = ''
 uuid = ''
 menuTitle = 'menu 1'
@@ -11,21 +10,23 @@ submenuTitleUpdate = 'new submenu 1'
 submenuDescriptionUpdate = 'new submenu description 1'
 
 
-def test_get_submenus():
-    response = client.post('http://fastapi_ylab:8000/api/v1/menus/', json={
+@pytest.mark.asyncio
+async def test_get_submenus(client, host):
+    response = await client.post(f'http://{host}:8000/api/v1/menus/', json={
         'title': menuTitle,
         'description': menuDescription
     })
     assert response.status_code == 201
     global uuidMenu
     uuidMenu = response.json()['id']
-    response = client.get('http://fastapi_ylab:8000/api/v1/menus/' + uuidMenu + '/submenus')
+    response = await client.get(f'http://{host}:8000/api/v1/menus/{uuidMenu}/submenus')
     assert response.status_code == 200
     assert response.json() == []
 
 
-def test_create_submenu():
-    response = client.post('http://fastapi_ylab:8000/api/v1/menus/' + uuidMenu + '/submenus', json={
+@pytest.mark.asyncio
+async def test_create_submenu(client, host):
+    response = await client.post(f'http://{host}:8000/api/v1/menus/{uuidMenu}/submenus', json={
         'title': submenuTitle,
         'description': submenuDescription
     })
@@ -34,40 +35,42 @@ def test_create_submenu():
     assert response.json()['description'] == submenuDescription
     global uuid
     uuid = response.json()['id']
-    response = client.get('http://fastapi_ylab:8000/api/v1/menus/' + uuidMenu + '/submenus')
+    response = await client.get(f'http://{host}:8000/api/v1/menus/{uuidMenu}/submenus')
     assert response.status_code == 200
-    assert response.json() != []
 
 
-def test_get_submenu():
-    response = client.get('http://fastapi_ylab:8000/api/v1/menus/' + uuidMenu + '/submenus/' + uuid)
+@pytest.mark.asyncio
+async def test_get_submenu(client, host):
+    response = await client.get(f'http://{host}:8000/api/v1/menus/{uuidMenu}/submenus/{uuid}')
     assert response.status_code == 200
     assert response.json()['title'] == submenuTitle
     assert response.json()['description'] == submenuDescription
 
 
-def test_update_submenu():
-    response = client.patch('http://fastapi_ylab:8000/api/v1/menus/' + uuidMenu + '/submenus/' + uuid, json={
+@pytest.mark.asyncio
+async def test_update_submenu(client, host):
+    response = await client.patch(f'http://{host}:8000/api/v1/menus/{uuidMenu}/submenus/{uuid}', json={
         'title': submenuTitleUpdate,
         'description': submenuDescriptionUpdate
     })
     assert response.status_code == 200
     assert response.json()['title'] == submenuTitleUpdate
     assert response.json()['description'] == submenuDescriptionUpdate
-    response = client.get('http://fastapi_ylab:8000/api/v1/menus/' + uuidMenu + '/submenus/' + uuid)
+    response = await client.get(f'http://{host}:8000/api/v1/menus/{uuidMenu}/submenus/{uuid}')
     assert response.status_code == 200
     assert response.json()['title'] == submenuTitleUpdate
     assert response.json()['description'] == submenuDescriptionUpdate
 
 
-def test_delete_submenu():
-    response = client.delete('http://fastapi_ylab:8000/api/v1/menus/' + uuidMenu + '/submenus/' + uuid)
+@pytest.mark.asyncio
+async def test_delete_submenu(client, host):
+    response = await client.delete(f'http://{host}:8000/api/v1/menus/{uuidMenu}/submenus/{uuid}')
     assert response.status_code == 200
-    response = client.get('http://fastapi_ylab:8000/api/v1/menus/' + uuidMenu + '/submenus')
+    response = await client.get(f'http://{host}:8000/api/v1/menus/{uuidMenu}/submenus')
     assert response.status_code == 200
     assert response.json() == []
-    response = client.get('http://fastapi_ylab:8000/api/v1/menus/' + uuidMenu + '/submenus/' + uuid)
+    response = await client.get(f'http://{host}:8000/api/v1/menus/{uuidMenu}/submenus/{uuid}')
     assert response.status_code == 404
     assert response.json()['detail'] == 'submenu not found'
-    response = client.delete('http://fastapi_ylab:8000/api/v1/menus/' + uuidMenu)
+    response = await client.delete(f'http://{host}:8000/api/v1/menus/{uuidMenu}')
     assert response.status_code == 200
