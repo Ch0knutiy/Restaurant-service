416618aa8325ad32ce562a66d158208a949fbb16
George Ustyanchik <ghkro123@gmail.com>
2023-08-21 03:16:03 +0700

    add schemas in docs

--- app/api/menu.py
+++ app/api/menu.py
@@ -18,17 +18,21 @@ async def get_xlsx_menus():
     return {'ok': True}
 
 
-@router.get('/full')
+@router.get('/full', response_model=list[schemas.ResponseMenusFull])
 async def get_menus_full(db: AsyncSession = Depends(get_session), rd: Redis = Depends(get_redis)):
     return await menuService.get_menus_full(db, rd)
 
 
-@router.get('/')
+@router.get('/', response_model=list[schemas.EnrichedMenuSchema])
 async def get_menus(db: AsyncSession = Depends(get_session), rd: Redis = Depends(get_redis)):
     return await menuService.get_menus(db, rd)
 
 
-@router.get('/{id}')
+@router.get('/{id}',
+            responses={
+                404: {'model': schemas.Message},
+                200: {'model': schemas.EnrichedMenuSchema}
+            })
 async def get_menu(id: UUID, db: AsyncSession = Depends(get_session), rd: Redis = Depends(get_redis)):
     menu = await menuService.get_menu(id, db, rd)
     if not menu:
@@ -37,13 +41,17 @@ async def get_menu(id: UUID, db: AsyncSession = Depends(get_session), rd: Redis 
     return menu
 
 
-@router.post('/', status_code=201)
+@router.post('/', response_model=schemas.ResponseMenu, status_code=201)
 async def create_menu(payload: schemas.MenuSchema, background_tasks: BackgroundTasks,
                       db: AsyncSession = Depends(get_session), rd: Redis = Depends(get_redis)):
     return await menuService.create_menu(payload, background_tasks, db, rd)
 
 
-@router.patch('/{id}', status_code=200)
+@router.patch('/{id}',
+              responses={
+                  404: {'model': schemas.Message},
+                  200: {'model': schemas.ResponseMenu}
+              })
 async def update_menu(id: UUID, payload: schemas.MenuSchema, background_tasks: BackgroundTasks,
                       db: AsyncSession = Depends(get_session), rd: Redis = Depends(get_redis)):
     menu = await menuService.update_menu(id, payload, background_tasks, db, rd)
@@ -53,7 +61,7 @@ async def update_menu(id: UUID, payload: schemas.MenuSchema, background_tasks: B
     return menu
 
 
-@router.delete('/{id}', status_code=200)
+@router.delete('/{id}', response_model=schemas.Delete, status_code=200)
 async def delete_menu(id: UUID, background_tasks: BackgroundTasks,
                       db: AsyncSession = Depends(get_session), rd: Redis = Depends(get_redis)):
     return await menuService.delete_menu(id, background_tasks, db, rd)
--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -41,3 +41,47 @@ class EnrichedSubmenuSchema(BaseModel):
     title: str
     description: str
     dishes_count: int
+
+
+class ResponseMenu(BaseModel):
+    id: UUID
+    title: str
+    description: str
+
+
+class ResponseSubmenu(BaseModel):
+    id: UUID
+    title: str
+    description: str
+    menu_id: UUID
+
+
+class ResponseDish(BaseModel):
+    id: UUID
+    title: str
+    description: str
+    price: str
+    submenu_id: UUID
+
+
+class Message(BaseModel):
+    detail: str
+
+
+class Delete(BaseModel):
+    ok: bool
+
+
+class ResponseSubmenusFull(BaseModel):
+    id: UUID
+    title: str
+    description: str
+    menu_id: UUID
+    dishes: list[ResponseDish]
+
+
+class ResponseMenusFull(BaseModel):
+    id: UUID
+    title: str
+    description: str
+    submenus: list[ResponseSubmenusFull]
